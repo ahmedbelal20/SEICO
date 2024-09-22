37e6ee9b8ab56b24c55dddf47bbbdc4322dfa58e
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2024-09-22 21:03:53 +0300

    feat: add override methods

--- include/messagedatamodel.h
+++ include/messagedatamodel.h
@@ -1,15 +1,23 @@
 #ifndef MESSAGEDATAMODEL_H
 #define MESSAGEDATAMODEL_H
 
-#include <QAbstractItemModel>
+#include <QAbstractTableModel>
 #include <QObject>
 
-class MessageDataModel : public QAbstractItemModel
+class MessageDataModel : public QAbstractTableModel
 {
+
     Q_OBJECT
 
+private:
+    int dataRowCount = 0;
+    const static int dataColumnCount = 8;
+
 public:
-    MessageDataModel(QObject *parent = nullptr);
+    explicit MessageDataModel(QObject *parent = nullptr);
+    inline int rowCount(const QModelIndex &parent = QModelIndex()) const override {return dataRowCount;}
+    inline int columnCount(const QModelIndex &parent = QModelIndex()) const override{return dataColumnCount;}
+
 };
 
 #endif // MESSAGEDATAMODEL_H
