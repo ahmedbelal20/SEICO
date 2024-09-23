ab8140b3183944d630dd9c3258529476458936ac
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2024-09-23 22:38:45 +0300

    feat: add columns enum

--- include/messagedatamodel.h
+++ include/messagedatamodel.h
@@ -9,14 +9,19 @@ class MessageDataModel : public QAbstractItemModel
 
     Q_OBJECT
 
+public:
+    enum ModelColumn {
+        Col_Time = 0, Col_Chn, Col_Id, Col_Name, Col_Event, Col_Dir, Col_Dlc, Col_Data, Col_Database
+    };
+
 private:
-    int dataRowCount = 0;
-    const static int dataColumnCount = 8;
+    int m_dataRowCount = 0;
+    const static int m_dataColumnCount = 9;
 
 public:
     explicit MessageDataModel(QObject *parent = nullptr);
-    inline int rowCount(const QModelIndex &parent = QModelIndex()) const override {return dataRowCount;}
-    inline int columnCount(const QModelIndex &parent = QModelIndex()) const override{return dataColumnCount;}
+    inline int rowCount(const QModelIndex &parent = QModelIndex()) const override {return m_dataRowCount;}
+    inline int columnCount(const QModelIndex &parent = QModelIndex()) const override{return m_dataColumnCount;}
 };
 
 #endif // MESSAGEDATAMODEL_H
