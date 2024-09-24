23cb4a317626cd38781dfa0a8c53aa7a04b5f0d5
Ahmed Belal <ahmedbelal.asurt@gmail.com>
2024-09-24 22:23:37 +0300

    feat: add model item class methods

--- include/messagemodelitem.h
+++ include/messagemodelitem.h
@@ -1,25 +1,34 @@
 #ifndef MESSAGEMODELITEM_H
 #define MESSAGEMODELITEM_H
 
-#include <QList>
+#include<QVector>
 
 #include "message.h"
 
+namespace DataModel {
 
-class MessageModelItem
-{
+    class MessageModelItem
+    {
 
-private:
-    QList<MessageModelItem*> m_childItems;
-    MessageModelItem *m_parentItem;
-    Message::CanMessage m_message;
+    private:
+        QVector<MessageModelItem*> m_childItems;
+        MessageModelItem *m_parentItem;
+        Message::CanMessage m_message;
+        char m_columnCount;
 
-public:
-    explicit MessageModelItem(const Message::CanMessage& message, MessageModelItem* parentItem);
-    ~ MessageModelItem() {}
-    QVariant data(int column) const;
+    public:
+        explicit MessageModelItem(const Message::CanMessage& message, MessageModelItem* parentItem);
+        // Other constructors can be added in the future for other types of messages (e.g. CAN FD, Ethernet, etc.)
+        ~ MessageModelItem();
 
-
+        void appendChild(MessageModelItem *child);
+        QVariant getData(int column) const;
+        MessageModelItem* getChild(int childNumber) const;
+        MessageModelItem* getParent() const;
+        int getChildCount() const;
+        int getColumnCount() const;
+        int getChildNumber() const;
+    };
 };
 
 #endif // MESSAGEMODELITEM_H
--- src/messagemodelitem.cpp
+++ src/messagemodelitem.cpp
@@ -4,39 +4,82 @@
 #include "messagedatamodel.h"
 
 
-MessageModelItem::MessageModelItem(const Message::CanMessage& message, MessageModelItem* parentItem = nullptr)
-    : m_message(message)
+DataModel::MessageModelItem::MessageModelItem(const Message::CanMessage& message, DataModel::MessageModelItem* parentItem = nullptr)
+    : m_message(message), m_parentItem(parentItem)
 {
-
+    switch(message.protocol)
+    {
+        case Message::Protocol::CAN:
+            m_columnCount = static_cast<char>(DataModel::CanMessageColumns::CAN_MESSAGE_COLUMNS_COUNT);
+            break;
+        default:
+            break;
+    }
 }
 
-QVariant MessageModelItem::data(int column) const
+DataModel::MessageModelItem::~MessageModelItem() {qDeleteAll(m_childItems);}
+
+void DataModel::MessageModelItem::appendChild(MessageModelItem *child) {m_childItems.append(child);}
+
+DataModel::MessageModelItem* DataModel::MessageModelItem::getChild(int childNumber) const
 {
-    switch(column)
+    if (childNumber < 0 || childNumber >= m_childItems.size())
+        return nullptr;
+    return m_childItems.at(childNumber);
+}
+
+DataModel::MessageModelItem* DataModel::MessageModelItem::getParent() const {return m_parentItem;}
+
+int DataModel::MessageModelItem::getChildCount() const {return m_childItems.count();}
+
+int DataModel::MessageModelItem::getColumnCount() const {return static_cast<int>(m_columnCount);}
+
+int DataModel::MessageModelItem::getChildNumber() const
+{
+    if (m_parentItem)
+        return m_parentItem->m_childItems.indexOf(const_cast<DataModel::MessageModelItem*>(this));
+    return 0;
+}
+
+QVariant DataModel::MessageModelItem::getData(int column) const
+{
+    switch(m_message.protocol)
     {
-        case MessageDataModel::Col_Id:
-            return QVariant(m_message.id);
-        case MessageDataModel::Col_Chn:
-            return QVariant(static_cast<int>(m_message.channel));
-        case MessageDataModel::Col_Data:
+        case Message::Protocol::CAN:
         {
-            QString hexString;
-            for (int i = 0; i < m_message.dlc; ++i) {
-                hexString += QString("%1 ").arg((m_message.data[i]), 2, 16, QChar('0')).toUpper();
+            switch(column)
+            {
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Id):
+                return QVariant(m_message.id);
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Chn):
+                return QVariant(static_cast<int>(m_message.channel));
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Data):
+            {
+                QString hexString;
+                for (int i = 0; i < m_message.dlc; ++i) {
+                    hexString += QString("%1 ").arg((m_message.data[i]), 2, 16, QChar('0')).toUpper();
+                }
+                return hexString.trimmed();
             }
-            return hexString.trimmed();
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Dir):
+                return QVariant(static_cast<int>(m_message.direction));
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Dlc):
+                return QVariant(m_message.dlc);
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Event):
+                return QString("CAN Frame");
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Time):
+                return QVariant(m_message.time);
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Database):
+                return QString::fromStdString(m_message.database);
+            case static_cast<int>(DataModel::CanMessageColumns::Col_Name):
+                return QString::fromStdString(m_message.name);
+            default:
+                return QVariant();
+            }
         }
-        case MessageDataModel::Col_Dir:
-            return QVariant(static_cast<int>(m_message.direction));
-        case MessageDataModel::Col_Dlc:
-            return QVariant(m_message.dlc);
-        case MessageDataModel::Col_Event:
-            return QString("CAN Frame");
-        case MessageDataModel::Col_Time:
-            return QVariant(m_message.time);
-        case MessageDataModel::Col_Database:
-            return QString::fromStdString(m_message.database);
-        case MessageDataModel::Col_Name:
-            return QString::fromStdString(m_message.name);
+        default:
+            return QVariant();
     }
+
+
 }
